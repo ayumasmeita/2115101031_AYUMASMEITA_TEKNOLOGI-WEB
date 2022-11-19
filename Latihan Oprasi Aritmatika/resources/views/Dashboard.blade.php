5d46d5485dcf16c4cbc1155a08265ba1a272928f
Gusti Putu Ayu Mas Meita Pradnya Swari <113962012+ayumasmeita@users.noreply.github.com>
2022-11-20 02:01:43 +0800

    Add files via upload

---
+++ Latihan Oprasi Aritmatika/resources/views/Dashboard.blade.php
@@ -0,0 +1,17 @@
+<x-admin-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7x1 mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:;rounded-lg">
+                <div class="p-6 bg-white border-b border-red-200 shadow-2x1 m-">
+                    selamat datang
+</div>
+</div>
+</div>
+</div>
+</x-admin-layout>
